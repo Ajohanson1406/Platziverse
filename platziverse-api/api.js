a10a3ddf02825fd22153a91a82782b0c89113c79
Albert <alberttruk1406@hotmail.es>
2020-09-17 19:18:41 -0600

    Adding the test to the api routes

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -1,32 +1,95 @@
 'use strict'
 
-const debug = require('debug')('platziverse:routes')
+const debug = require('debug')('platziverse:api:routes')
 const express = require('express')
+const asyncify = require('express-asyncify')
+const db = require('platziverse-db')
 
-const api = express.Router()
+const { config } = require('platziverse-tools')
+const { agent } = require('supertest')
 
-api.get('/agents', (req, res) => {
-  debug('A request has come to /agents')
-  res.send({})
+const api = asyncify(express.Router())
+
+let services, Agent, Metric
+
+api.use('*', async (req, res, next) => {
+  debug('Connecting to DB')
+  if (!services) {
+    try {
+      services = await db(config.db)
+    } catch (error) {
+      return next(error)
+    }
+    Agent = services.Agent
+    Metric = services.Metric
+  }
+  next()
 })
 
-api.get('/agent/:uuid', (req, res, next) => {
+api.get('/agents', async(req, res, next) => {
+ let agents = []
+
+ try {
+   agents = await Agent.findConnected()
+ } catch (e) {
+   return next(e)
+   
+ }
+  res.send(agents)
+})
+
+api.get('/agent/:uuid', async (req, res, next) => {
   const { uuid } = req.params
 
-  if(uuid != 'yyy') {
-      return next(new Error('Agent not found'))
+  debug(`Request to /agent/${uuid}`)
+
+  let agent
+  try {
+    agent = await Agent.findByUuid(uuid)
+  } catch (e) {
+    return next(e)
+  }
+  if (!agent) {
+    return next(new Error(`Agent not found with [uuid] ${uuid}`))
   }
 
-  res.send({ uuid })
+  res.send(agent)
 })
 
-api.get('/metrics/:uuid', (req, res) => {
+api.get('/metrics/:uuid', async(req, res, next) => {
   const { uuid } = req.params
+
+  debug(`Request to /metric/${uuid}`)
+
+  let metric = []
+
+  try {
+    metric = await Metric.findByUuid(uuid)
+  } catch (e) {
+    return next(e)
+  }
+
+  if(!metric || metric.length === 0) {
+    return next(new Error(`Metrics not found to the agent with uuid ${uuid}`))
+  }
   res.send({ uuid })
 })
 
-api.get('/metrics/:uuid/:type', (req, res) => {
+api.get('/metrics/:uuid/:type', async(req, res, next) => {
   const { uuid, type } = req.params
+
+  debug(`Request to /metrics/${uuid}/${type}`)
+
+  let metrics = []
+
+  try {
+    metrics = await Metric.findByTypeAgentUuid(type, uuid)
+  } catch (e) {
+    return next(e)
+  }
+  if(!metric || metric.length === 0) {
+    return next(new Error(`Metrics not found to the agent with uuid ${uuid} and type ${type}`))
+  }
   res.send({ uuid, type })
 })
 
--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -6,8 +6,8 @@ const http = require('http')
 const express = require('express')
 
 const {
-    utils: { handleFatalError }
-  } = require('platziverse-tools')
+  utils: { handleFatalError }
+} = require('platziverse-tools')
 
 const api = require('./api')
 
@@ -17,21 +17,25 @@ const server = http.createServer(app)
 
 app.use('/api', api)
 
-//Express Error Handler
+// Express Error Handler
 
 app.use((err, req, res, next) => {
-    debug(`Error: ${err.message}`)
+  debug(`Error: ${err.message}`)
 
-    if(err.message.match(/not found/)) {
-        return res.status(404).send({ error: err.message})
-    }
+  if (err.message.match(/not found/)) {
+    return res.status(404).send({ error: err.message })
+  }
 
-    res.status(500).send({error: err.message})
+  res.status(500).send({ error: err.message })
 })
 
-process.on('uncaughtException', handleFatalError)
-process.on('unhandledRejection', handleFatalError)
+if (!module.parent) {
+  process.on('uncaughtException', handleFatalError)
+  process.on('unhandledRejection', handleFatalError)
 
-server.listen(port, () => {
-  console.log(`${chalk.green('Server is running in port 3000')}`)
-})
+  server.listen(port, () => {
+    console.log(`${chalk.green('Server is running in port 3000')}`)
+  })
+}
+
+module.exports = server
